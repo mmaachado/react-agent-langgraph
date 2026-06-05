ac1e2b61420e016337fd5ff092a994fcd8cba8b2
Marcelo Machado <uniqueduckinbox@duck.com>
2026-06-05 18:47:27 -0300

    repo backup

--- src/graph.py
+++ src/graph.py
@@ -1,42 +1,60 @@
 import operator
-from typing import Annotated, TypedDict
+from dataclasses import dataclass
+from typing import Annotated, Literal, Literals
 
+from langgraph.constants import END, START
 from langgraph.graph import StateGraph
 
-# def reducer(a: list[str], b: list[str]) -> list[str]:
-# 	return a + b
 
-
-class State(TypedDict):
+@dataclass
+class State:
 	nodes_path: Annotated[list[str], operator.add]
+	current_number: int = 0
 
 
 def node_a(state: State) -> State:
 
-	output_state: State = {'nodes_path': ['A']}
+	output_state: State = State(nodes_path=['A'], current_number=state.current_number)
 	print('> node_a', f'{state=}', f'{output_state=}')
 	return output_state
 
 
 def node_b(state: State) -> State:
 
-	output_state: State = {'nodes_path': ['B']}
+	output_state: State = State(nodes_path=['B'], current_number=state.current_number)
 	print('> node_b', f'{state=}', f'{output_state=}')
 	return output_state
+
+
+def node_c(state: State) -> State:
+
+	output_state: State = State(nodes_path=['C'], current_number=state.current_number)
+	print('> node_c', f'{state=}', f'{output_state=}')
+	return output_state
+
+
+def conditional(state: State) -> Literal['B', 'C']:
+	if state.current_number >= 50:
+		return 'C'
+	return 'B'
 
 
 builder = StateGraph(State)
 
 builder.add_node('A', node_a)
 builder.add_node('B', node_b)
+builder.add_node('C', node_c)
 
 
-builder.add_edge('__start__', 'A')
-builder.add_edge('A', 'B')
-builder.add_edge('B', '__end__')
+builder.add_edge(START, 'A')
+builder.add_conditional_edges('A', conditional, ['B', 'C'])
+builder.add_edge('B', END)
+builder.add_edge('C', END)
 
 graph = builder.compile()
 
-response = graph.invoke({'nodes_path': []})
+response = graph.invoke(State(nodes_path=[]))
 
-# graph.get_graph().draw_mermaid_png(output_file_path='file.png')
+print()
+print(f'{response=}')
+print()
